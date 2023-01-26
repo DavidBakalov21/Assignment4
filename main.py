5119021d0c64f64069493b936796943f8693392a
David Bakalov <davidbakalov33@gmail.com>
2023-01-26 18:37:26 +0200

    first steps

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-import  matplotlib as mtp
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 #a = np.array([1, 2, 3, 4, 5, 6])
 
 
@@ -23,34 +24,3 @@ def first():
     plt.xlabel("Score")
     plt.ylabel("titles")
     plt.show()
-def second():
-    AgeData=file[file["type"]=="SHOW"].dropna()
-    readyData=AgeData.pivot_table(columns=['age_certification'], aggfunc='size')
-    readyDataFrame=readyData.reset_index()
-    LabelData=readyDataFrame["age_certification"]
-    print(readyData)
-    plt.pie(readyData, labels=LabelData)
-    plt.show()
-
-
-def third():
-    DateData=file[file["release_year"]>=2000].dropna()
-    DateDataRate=DateData[DateData["imdb_score"]>=8.0].dropna()
-    XData=DateDataRate["release_year"]
-    YData=DateDataRate["imdb_score"]
-    #xpoints = np.array([0, 6])
-    #ypoints = np.array([0, 250])
-    print(XData.shape)
-    print(YData.shape)
-    plt.scatter(XData, YData)
-    plt.show()
-
-
-    #print(DateData)
-
-#class_23 = titanic[titanic["it"].isin(["23"])]
-
-#print(mL)
-#third()
-second()
-#print(titanic.tail(10))
